61d2442b0889ca05f58febb68bf277d85d291bcf
Balou <Balou@PC-BALOU>
2021-12-28 11:41:44 +0100

    ajout de commentaires dans la classe Controler

--- src/controler/Controler.java
+++ src/controler/Controler.java
@@ -1,7 +1,14 @@
 
 package controler;
 
+
 import utilitairesMK.ConsoleMK;
+
+
+/**
+ * Classe Interface pour creer un Controleur d'application
+ * dans un modele MVC 
+ */
 
 public interface Controler {
 	
